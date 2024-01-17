272520299f22f0560ba9fb7e8e0ec11fbe4dedde
Jamie897 <jamiedanielle456@gmail.com>
2024-01-17 10:54:02 -0600

    Trying

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -2,40 +2,30 @@
 """
 Route module for the API
 """
-
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-# Create instance of Auth if AUTH_TYPE is set to 'auth'
-auth = None
-auth_type = getenv("AUTH_TYPE", "basic")
-if auth_type == "auth":
-    from api.v1.auth.auth import Auth
-    auth = Auth()
 
-if auth_type == "basic_auth":
+# Initialize auth to None
+auth = None
+
+# Get AUTH_TYPE from environment
+AUTH_TYPE = os.getenv('AUTH_TYPE')
+
+# Conditionally create a BasicAuth instance based on AUTH_TYPE
+if AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-
-
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """Unauthorized handler
-    """
-    return jsonify({"error": "Unauthorized"}), 401
-
-
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """Forbidden handler
-    """
-    return jsonify({"error": "Forbidden"}), 403
+elif AUTH_TYPE:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -45,19 +35,46 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
+@app.errorhandler(401)
+def unauthorized_error(error) -> str:
+    """ Not authorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden_error(error) -> str:
+    """ forbidden handler """
+    return jsonify({"error": "Forbidden"}), 403
+
+
 @app.before_request
-def before_request_func() -> str:
-    """ Before request handler
+def before_request_func() -> None:
+    """ Before request handler function
+            before_request_func = None
+            A dictionary with lists of functions that will be called
+            at the beginning of each request.
+            The key of the dictionary is the name of the blueprint
+            this function is active for,
+            or None for all requests.
+            To register a function, use the before_request() decorator.
     """
     if auth is None:
         return
+
+    # paths to exclude from authentication
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
                       '/api/v1/forbidden/']
+    # check if request path is excluded
     if not auth.require_auth(request.path, excluded_paths):
         return
+
+    # check if request is not authenticated
     if auth.authorization_header(request) is None:
         abort(401)
+
+    # check if request is not authenticated
     if auth.current_user(request) is None:
         abort(403)
 
--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -1,27 +1,24 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
-from flask import jsonify, abort, Blueprint
+from flask import jsonify, abort
 from api.v1.views import app_views
-
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
     Return:
-      - the status of the API
+    - the status of the API
     """
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats/', strict_slashes=False)
+@app_views.route('/stats/', strict_slashes=False, methods=['GET'])
 def stats() -> str:
     """ GET /api/v1/stats
     Return:
-      - the number of each objects
+    - the number of each objects
     """
     from models.user import User
     stats = {}
@@ -29,16 +26,23 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+@app_views.route('/unauthorized/', strict_slashes=False, methods=['GET'])
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
+    Return:
+    - abort(401)
+    401 means “Unauthorized”, but you redirect
+    to the login page instead of returning that status
     """
     abort(401)
 
 
-@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+@app_views.route("/forbidden/", strict_slashes=False, methods=['GET'])
 def forbidden() -> str:
     """ GET /api/v1/forbidden
+    Returns:
+        str: abort(403)
+        403 means “Forbidden”,
     """
     abort(403)
     
