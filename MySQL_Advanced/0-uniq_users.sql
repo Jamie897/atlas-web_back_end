8266d92c77ff47f21b1f652f30385d32e597df73
Jamie897 <jamiedanielle456@gmail.com>
2024-02-06 10:56:15 -0600

    redo

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,9 @@
+-- Ensure that the table is created only if it doesn't exist
 CREATE TABLE IF NOT EXISTS users (
+    -- Define the id column as an auto-incrementing primary key
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    -- Define the email column as a string with a maximum length of 255 characters, not nullable, and with a unique constraint
     email VARCHAR(255) NOT NULL UNIQUE,
+    -- Define the name column as a string with a maximum length of 255 characters
     name VARCHAR(255)
 );
