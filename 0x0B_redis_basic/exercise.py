6ee5e75a23be9335e55ccbef45725aec646578c3
Jamie897 <jamiedanielle456@gmail.com>
2024-02-06 10:17:32 -0600

    Again

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -6,17 +6,12 @@ from typing import Union
 
 class Cache:
     def __init__(self):
-        # Store an instance of the Redis client as a private variable named _redis
         self._redis = redis.Redis()
-        # Flush the Redis instance using flushdb
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        # Generate a random key using uuid
         key = str(uuid.uuid4())
-        # Store the input data in Redis using the random key
         self._redis.set(key, data)
-        # Return the key
         return key
 
 # Main code
