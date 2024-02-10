f55243674874abcb70442362612e631eccd81bdb
Jamie897 <jamiedanielle456@gmail.com>
2024-02-10 14:51:17 -0600

    Making Main

---
+++ MySQL_Advanced/2-main.sql
@@ -0,0 +1,15 @@
+bob@dylan:~$ cat metal_bands.sql | mysql -uroot -p holberton
+Enter password: 
+bob@dylan:~$ 
+bob@dylan:~$ cat 2-fans.sql | mysql -uroot -p holberton > tmp_res ; head tmp_res
+Enter password: 
+origin  nb_fans
+USA 99349
+Sweden  47169
+Finland 32878
+United Kingdom  32518
+Germany 29486
+Norway  22405
+Canada  8874
+The Netherlands 8819
+Italy   7178
