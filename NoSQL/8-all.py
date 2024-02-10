05d266dfdb69334e89b8342fb4f7a30f41c6e43d
Jamie897 <jamiedanielle456@gmail.com>
2024-02-10 16:12:47 -0600

    Import Mongo

---
+++ NoSQL/8-all.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import pymongo
+
+def list_all(mongo_collection):
+    """ List all documents in a collection """
+    return mongo_collection.find() if mongo_collection else []
