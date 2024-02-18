a02cef81822f5d402cf3d5b1ffddc75d5ddf3a46
Jamie897 <jamiedanielle456@gmail.com>
2024-02-18 17:26:45 -0600

    Task 3

---
+++ unittests_in_js/3-payment.js
@@ -0,0 +1,10 @@
+// 3. 
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const sum = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${sum}`);
+  return sum;
+}
+
+module.exports = sendPaymentRequestToApi;
